69311d60f78a1969cfa3e3def40db64ffc8120e7
Bob Argenbright <bargenbright@bargenbri-ltm1.internal.salesforce.com>
2016-07-28 08:51:48 -0700

    chose K-NN algorithm and completed

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
-from class_vis import prettyPicture
+#from class_vis import prettyPicture
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -17,6 +17,7 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
+"""
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
 plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
@@ -25,20 +26,26 @@ plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
+"""
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+clf = KNeighborsClassifier(n_neighbors=1)
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
+accuracy = accuracy_score(labels_test, pred)
 
+print accuracy
 
-
-
-
-
+"""
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+"""
